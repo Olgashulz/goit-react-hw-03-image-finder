0026fca312d85b0a02084a2dc815aef59d86752e
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-10-06 15:02:10 +0300

    структура + Searchbar

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
+
+export default class App extends Component {
+  state = {
+    inputValue: '',
+    images: {},
+    loading: false,
+  };
+
+  handleSearchSubmit = inputValue => {
+    console.log(inputValue);
+    this.setState({ inputValue });
+  };
+
+  // componentDidMount() {
+  //     // fetch('https://pixabay.com/api/?key=10507999-623e060cae639baa9b9819f90&q=flowers&image_type=photo')
+  //     //     .then(res => res.json())
+  //     //     .then(console.log);
+  //     this.setState({ loading: true })
+
+  //     fetch('https://pixabay.com/api/?key=10507999-623e060cae639baa9b9819f90&q=flowers&image_type=photo')
+  //         .then(res => res.json())
+  //         .then(image => this.setState({ image }))
+  //         .finally(() => this.setState({ loading: false }));
+
+  // }
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        {this.state.loading && <h1>Загружаем...</h1>}
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery inputValue={this.state.inputValue} />
+        <ToastContainer autoClose={2500} />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default class Searchbar extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleInputValueChange = event => {
+    // console.log(event.currentTarget.value.toLowerCase())
+    this.setState({ inputValue: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.inputValue.trim() === '') {
+      return toast.error('Please enter keyword.', {});
+    }
+
+    this.props.onSubmit(this.state.inputValue);
+    this.setState({ inputValue: '' });
+  };
+
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleOnSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleInputValueChange}
+            // autocomplete="off"
+            // autofocus
+            // placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
