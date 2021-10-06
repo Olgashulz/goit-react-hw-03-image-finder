e03a91a34942f9064248abde13ff6d699578e2a3
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-10-06 15:50:23 +0300

    структура + Searchbar + ImageGallery

--- src/App.js
+++ src/App.js
@@ -19,18 +19,17 @@ export default class App extends Component {
   //     // fetch('https://pixabay.com/api/?key=10507999-623e060cae639baa9b9819f90&q=flowers&image_type=photo')
   //     //     .then(res => res.json())
   //     //     .then(console.log);
-  //     this.setState({ loading: true })
 
-  //     fetch('https://pixabay.com/api/?key=10507999-623e060cae639baa9b9819f90&q=flowers&image_type=photo')
-  //         .then(res => res.json())
-  //         .then(image => this.setState({ image }))
-  //         .finally(() => this.setState({ loading: false }));
+  //     this.setState({ loading: true })
+  //   fetch('https://pixabay.com/api/?key=10507999-623e060cae639baa9b9819f90&q=flowers&image_type=photo')
+  //       .then(res => res.json())
+  //       .then(image => this.setState({ image }))
+  //       .finally(() => this.setState({ loading: false }));
 
   // }
   render() {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
-        {this.state.loading && <h1>Загружаем...</h1>}
         <Searchbar onSubmit={this.handleSearchSubmit} />
         <ImageGallery inputValue={this.state.inputValue} />
         <ToastContainer autoClose={2500} />
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 
 export default class ImageGallery extends Component {
+  state = {
+    images: [],
+    loading: false,
+  };
+
+  componentDidUpdate(prevProps, PrevState) {
+    const inputValue = this.props.inputValue;
+    if (prevProps.inputValue !== inputValue) {
+      this.setState({ loading: true });
+      fetch(
+        `https://pixabay.com/api/?key=10507999-623e060cae639baa9b9819f90&q=${inputValue}&image_type=photo`,
+      )
+        .then(res => res.json())
+        .then(images => this.setState({ images: images.hits }))
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
   render() {
-    return <div>HI</div>;
+    return (
+      <ul className="ImageGallery">
+        {this.state.loading && <h1>Загружаем...</h1>}
+      </ul>
+    );
   }
 }
